899fde6d33d3dc26798928b68ef31bee0f4ff3a9
Mattias Thander <mattias@devoid.se>
2014-12-18 11:35:38 +0100

    Lagt till Felhantering

---
+++ Exempel/8. Felhantering med try-catch/Felhantering/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Felhantering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal = 0;
+
+            while(true)
+            {
+                try
+                {
+                    Console.Write("Ange ett heltal: ");
+                    tal = int.Parse(Console.ReadLine());
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Du måste ange ett heltal!");
+                }
+
+            }
+          // efter break
+
+
+
+
+            Console.WriteLine("Inmatat tal: {0}", tal);
+
+        }
+    }
+}
