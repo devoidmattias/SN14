d1fb9335a6d495411aea5c3aeb8ed8c916ae30ea
Mattias Thander <mattias@devoid.se>
2014-12-18 11:01:25 +0100

    Lagt till fler exempel

---
+++ Exempel/6. Knackningar med while-loop/Knacka/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Knacka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int antalKnackningar = 0;
+
+            // mata in värde
+            Console.Write("Hur många knackningar? ");
+            antalKnackningar = int.Parse(Console.ReadLine());
+
+          
+
+            // initierar en kontrollvariabel
+            while(antalKnackningar > 0)
+            {
+                Console.WriteLine("Knack");
+
+                // uppdatera kontrollvariabeln
+                antalKnackningar--;
+
+            }
+            Console.WriteLine("Klart!");
+           
+
+        }
+    }
+}
---
+++ Exempel/7. Summera med while-loop/Summera/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = 0;
+            int tal = 0;
+
+            // mata in värde
+            Console.Write("Ange heltal som skall summeras: ");
+            tal = int.Parse(Console.ReadLine());
+           
+            while (tal != 0)
+            {
+                summa += tal;
+                tal = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Summan är {0}!", summa);
+        }
+    }
+}
